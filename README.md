998f6ed1f313bf6742e750878b52ec72474cfac5
loosand <loosand@163.com>
2023-09-07 23:44:27 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Vue 3 + TypeScript + Vite + Tailwind + Sass + Pinia + Axios + Mock.js
+
+> 这个模板可以帮助你开始在Vite中使用Vue3和TypeScript进行开发。
---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <h1 class="text-5xl font-bold">Vue Template Package</h1>
+</template>
+
+<style scoped>
+
+</style>
